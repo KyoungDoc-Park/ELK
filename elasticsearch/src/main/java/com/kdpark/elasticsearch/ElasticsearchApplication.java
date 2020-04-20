6cd603e599e3bc75bdee2e8b5b1cc9dae229497f
KyoungDoc-Park <kyoungduck83@naver.com>
2020-04-20 18:19:24 +0900

    add

--- elasticsearch/src/main/java/com/kdpark/elasticsearch/ElasticsearchApplication.java
+++ elasticsearch/src/main/java/com/kdpark/elasticsearch/ElasticsearchApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ElasticsearchApplication {
 
 	public static void main(String[] args) {
+		// add juseok
 		SpringApplication.run(ElasticsearchApplication.class, args);
 	}
 }
